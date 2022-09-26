cf8bb2b7226b407f51f81c501201743fd179c64f
Ilya Kubariev <ilya.kubariev@redduck.io>
2022-09-26 21:00:27 +0300

    feat: Delete Occurrences Of An Element If It Occurs More Than N Times

---
+++ Codewars.DeleteOccurrencesOfAnElementIfItOccursMoreThanNTimes/Program.fs
@@ -0,0 +1,11 @@
+﻿let deleteNth array max_e =
+    let cache = System.Collections.Generic.Dictionary<int, int> ()
+    array |> List.filter (fun i ->
+        cache.[i] <- if cache.ContainsKey i then cache.[i] + 1 else 1
+        cache.[i] <= max_e
+    )
+    
+[<EntryPoint>]
+let main _ =
+    deleteNth [1; 1; 3; 3; 3; 3; 1; 1; 7; 2; 2; 2; 2] 3 |> printfn "%A"
+    0
