928077722c999ed05a2cfe6c46d04829022db362
Ilya Kubariev <ilya.kubariev@redduck.io>
2022-09-27 01:39:12 +0300

    feat: Most Frequent Weekdays

---
+++ Codewars.MostFrequentWeekdays/Program.fs
@@ -0,0 +1,17 @@
+﻿let mostFrequentDays (year: int) =
+    let day = System.DateTime(year, 1, 1)
+    if System.DateTime.IsLeapYear year then
+        if day.DayOfWeek = System.DayOfWeek.Sunday then
+            [ "Monday"; "Sunday" ]
+        else
+            [ day.ToString("dddd", System.Globalization.CultureInfo "en-US")
+              day.AddDays(1.0).ToString("dddd", System.Globalization.CultureInfo "en-US") ]
+    else [ day.ToString("dddd", System.Globalization.CultureInfo "en-US") ]
+
+
+    
+[<EntryPoint>]
+let main _ =
+    mostFrequentDays 1984 |> printfn "%A"
+    mostFrequentDays 2016 |> printfn "%A"
+    0
