5ab63604fc6055d7a898c05f5a51d340042f56aa
Ilya Kubariev <ilya.kubariev@redduck.io>
2022-09-26 20:27:43 +0300

    feat: Highest Rank Number in an Array

---
+++ Codewars.HighestRankNumberInAnArray/Program.fs
@@ -0,0 +1,9 @@
+﻿open System.Linq
+
+let highestRank (array: int[]) =
+    array |> Array.sortDescending |> Array.maxBy (fun i -> array.Count(fun j -> i = j))
+
+[<EntryPoint>]
+let main _ =
+    highestRank [|3; 3; 3; 12; 10; 8; 12; 7; 6; 4; 10; 12|] |> printfn "%A"
+    0
