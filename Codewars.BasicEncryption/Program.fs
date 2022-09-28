3e0821e8e042ac8c9211cf491accef06bcae7232
Ilya Kubariev <ilya.kubariev@redduck.io>
2022-09-28 14:46:24 +0300

    feat: Basic Encryption

---
+++ Codewars.BasicEncryption/Program.fs
@@ -0,0 +1,10 @@
+﻿let encrypt (rule : int) (str : string) : string = 
+    str
+    |> Seq.map (fun c -> c |> int |> (+) rule |> fun i -> i % 256 |> char)
+    |> System.String.Concat
+    
+[<EntryPoint>]
+let main _ =
+    encrypt 2 "please encrypt me" |> printfn "%A"
+    encrypt 1 "a" |> printfn "%A"
+    0
