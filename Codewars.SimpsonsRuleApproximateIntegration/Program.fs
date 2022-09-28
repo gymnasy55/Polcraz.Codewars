10c93002a03c4124e6e7d3a129af01eb2b428742
Ilya Kubariev <ilya.kubariev@redduck.io>
2022-09-28 19:46:41 +0300

    feat: Simpson's Rule - Approximate Integration

---
+++ Codewars.SimpsonsRuleApproximateIntegration/Program.fs
@@ -0,0 +1,26 @@
+﻿let simpson (n: int) : float =
+    let f x = (3.0 / 2.0) * pown (sin x) 3
+    let h x = System.Math.PI / (x * (n |> float))
+
+    f 0.0
+    |> (+) (f System.Math.PI)
+    |> (+)
+        (
+            [ for i in 1 .. (n / 2) -> ((i |> float) * 2.0 - 1.0) * h 1.0 ]
+            |> List.map f
+            |> List.sum
+            |> (*) 4.0
+        )
+    |> (+)
+        (
+            [ for i in 1 .. (n / 2) - 1 -> ((i |> float) * 2.0) * h 1.0 ]
+            |> List.map f
+            |> List.sum
+            |> (*) 2.0
+        )
+    |> (*) (h 3.0)
+
+[<EntryPoint>]
+let main _ =
+    simpson 10 |> printfn "%A"
+    0
